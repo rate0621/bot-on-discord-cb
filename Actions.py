228a3b9f1bee9b6e6411e7e0fd5da84bba854adf
ebina <ebina@ebinanoMacBook-puro.local>
2018-10-05 16:04:17 +0900

    commit

--- Actions.py
+++ Actions.py
@@ -1,4 +1,5 @@
 import os, re
+from datetime import datetime
 
 import common_lib.priconne_gacha_simulator.GachaSimulation as GachaSimulation
 import common_lib.priconne_gacha_simulator.ImageGenerator  as ImageGenerator
@@ -11,6 +12,8 @@ class Actions:
     self.res_type = None
 
   def check_and_response(self, req):
+    here = os.path.join( os.path.dirname(os.path.abspath(__file__)))
+
     # スタンプを返す系以外のは初めにチェックする
     if re.match("^プリコネ\sガチャ", req.content):
       self.res_type = 'file'
@@ -30,22 +33,32 @@ class Actions:
 
       return self.res_type, self.res
 
-    elif re.match("^草", req.content):
-      self.res_type = 'file'
-      here          = os.path.join( os.path.dirname(os.path.abspath(__file__)))
-      self.res      = here + "/static/other/kusaa.jpg"
-
-      return self.res_type, self.res
+#    elif re.match("^草", req.content):
+#      self.res_type = 'file'
+#      self.res      = here + "/static/other/kusaa.jpg"
+#
+#      return self.res_type, self.res
 
     # スタンプ系はこの下に記述していく
-    for word_list in Pri.responses:
-      for word in word_list.split(','):
-        if re.search(word, req.content):
-          here          = os.path.join( os.path.dirname(os.path.abspath(__file__)))
-          self.res      = here + "/static/priconne/" + Pri.responses[word_list]
-          self.res_type = 'file'
 
-          return self.res_type, self.res
+    # 連続でスタンプがでまくるとログが流れてしまったりと鬱陶しいので、
+    # スタンプは5分間に1回しかださない
+    last_stamp_time_fp = here + '/last_stamp_time.txt'
+    with open(last_stamp_time_fp, mode='r') as fh:
+        last_stamp_time = datetime.strptime(fh.read(), '%Y/%m/%d %H:%M:%S')
+
+    now = datetime.now()
+    if (now - last_stamp_time).total_seconds() >= 500:
+      for word_list in Pri.responses:
+        for word in word_list.split(','):
+          if re.search(word, req.content):
+            self.res      = here + "/static/priconne/" + Pri.responses[word_list]
+            self.res_type = 'file'
+
+            with open(last_stamp_time_fp, mode='w') as fh:
+              fh.write(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+
+            return self.res_type, self.res
 
     ## 絵文字系はこの下に記述
     if re.search('幼女', req.content):
