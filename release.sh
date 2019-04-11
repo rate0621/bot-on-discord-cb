59b376400e7d6ac640dc4153b3a70dd6199f6777
ebina <ebina@ebinanoMacBook-puro.local>
2019-04-11 21:10:45 +0900

    プロセスとして実行できるように起動スクリプトなどを追加

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+source secret/env.sh
+
+## 実行中プロセスの停止
+ps aux | grep discord_bot | grep -v grep | awk '{ print "kill -9", $2 }' | sh
+
+
+git pull origin master
+nohup python discord_bot.py > out.log 2> err.log &
+
