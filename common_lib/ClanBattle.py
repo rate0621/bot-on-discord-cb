0d5c3538000966027ec988621f123b71a94a734f
蛯名麗斗 <rate@ebinareitonoMacBook-Pro.local>
2021-05-26 12:27:18 +0900

    5段階目に入るタイミングも変わったため更新

--- common_lib/ClanBattle.py
+++ common_lib/ClanBattle.py
@@ -552,9 +552,9 @@ class ClanBattle():
             return 2
         elif loop_count >= 11 and loop_count <= 30:
             return 3
-        elif loop_count >= 31 and loop_count <= 44:
+        elif loop_count >= 31 and loop_count <= 40:
             return 4
-        elif loop_count >= 45:
+        elif loop_count >= 41:
             return 5
         else:
             return 0
